81ed0b3922e32792c31a032ddede3a79ce2d134e
Robert Glissmann <rgmann@users.noreply.github.com>
2015-09-28 22:28:50 -0600

    Delete a bunch dead code generation code.

--- utils/rpc_generate/source/block_parser/cast_test.rb
+++
@@ -1,9 +0,0 @@
-require 'cast'
-
-source = File.read(ARGV[0])
-ast = C.parse(source)
-ast.entities.each do |declaration|
-  declaration.declarator.each do |declarator|
-    puts "#{declarator.name}: declarator.type"
-  end
-end
--- utils/rpc_generate/source/block_parser/test_defs_2.h
+++
@@ -1,9 +0,0 @@
-struct __attribute__ ((__packed__)) another_struct {
-  union {
-    uint8 bytes[30];
-    struct {
-      unsigned a : 2;
-      unsigned b : 5;
-    } bits;
-  } data;
-} another_struct_t;
