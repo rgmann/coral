7bda7e2cb869529de7f41225a1e1db7f911d803b
Robert Glissmann <rgmann@users.noreply.github.com>
2015-09-30 22:42:53 -0600

    Removed in progress event replay library.

--- test/replay/replay_test_suite.cpp
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include "gtest/gtest.h"
-#include "replay_event_log_test.cpp"
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
-
